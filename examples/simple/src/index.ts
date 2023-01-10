88ab316a8cf837ba1a020b3faed476d4adc17ecf
Tyler Flint <tylerflint@gmail.com>
2023-01-10 13:57:49 -0500

    Expose capabilities for masking assets, don't proxy assets by default

--- examples/simple/src/index.ts
+++ examples/simple/src/index.ts
@@ -5,14 +5,15 @@ import rewriteHtml from "@qpoint/rewrite-html";
 
 export default new Router()
   // proxy to qpoint.io
-  // .use(proxy({ appUrl: "https://qpoint.io" }))
+  .use(proxy({ appUrl: "https://qpoint.io" }))
 
   // proxy to www.newegg.com
-  .use(proxy({ appUrl: "https://www.newegg.com" }))
+  // .use(proxy({ appUrl: "https://www.newegg.com" }))
 
   // mask the urls to match the proxy endpoint
   .use(maskUrls({
-    absolute: true
+    absolute: true,
+    // maskAssets: true,
   }))
 
   // activate the rewrite
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Context } from '@qpoint/router'
 import { UrlRewriter } from './url-rewriter'
 
 export interface MaskUrlsConfig {
-  basePath?: string   // if the site routing is pinned to a basePath, like site.com/docs or site.com/blog
-  absolute?: boolean  // should all links be converted to absolute, like //www.site.com/products/1
-  edgeUrl? : string   // use this edge url, rather than the url from the request
-  appUrl?  : string   // use this app url, rather than the url from the proxy
+  basePath?  : string   // if the site routing is pinned to a basePath, like site.com/docs or site.com/blog
+  absolute?  : boolean  // should all links be converted to absolute, like //www.site.com/products/1
+  edgeUrl?   : string   // use this edge url, rather than the url from the request
+  appUrl?    : string   // use this app url, rather than the url from the proxy
+  maskAssets?: boolean  // whether to also mask resources, assets, images, and scripts
 }
 
 // adapter registration
@@ -26,12 +27,21 @@ export default function rewriteHtml(config: MaskUrlsConfig = {}) {
     if (ctx.state['mask-urls.config'])
       config = ctx.state['mask-urls.config'] as MaskUrlsConfig
 
-    // attach to the rewriter
+    // generate an assets config
+    const assetsConfig = {
+      ...config,
+      ...( !config.maskAssets && { 
+        absolute: true, 
+        edgeUrl: config.edgeUrl || config.appUrl || ctx.pxy.url
+      }),
+    }
+
+    // add rewrite rules
     ctx.htmlRewriter
       .on("a", new UrlRewriter('href', ctx, config))
-      .on("link", new UrlRewriter('href', ctx, config))
-      .on("img", new UrlRewriter('src', ctx, config))
-      .on("script", new UrlRewriter('src', ctx, config))
+      .on("link", new UrlRewriter('href', ctx, assetsConfig))
+      .on("img", new UrlRewriter('src', ctx, assetsConfig))
+      .on("script", new UrlRewriter('src', ctx, assetsConfig))
 
     // continue along
     return next();
